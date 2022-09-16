e72af5f31555d795033f724c7310da4a90876b6d
Javier <javierblancoit@gmail.com>
2022-09-16 19:01:17 +0100

    create urls and views to return a single movie upon a get request by id

--- watchlist_app/urls.py
+++ watchlist_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from watchlist_app.views import movie_list
+from watchlist_app import views
 
 urlpatterns = [
-    path('list/', movie_list, name= 'movie-list')
+    path('list/', views.movie_list, name= 'movie-list'),
+    path('<int:pk>', views.movie_details, name= 'movie-details') # if there is any integer, it is gonna be send to the view as pk
 ]
--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -6,7 +6,6 @@ from django.http import JsonResponse
 
 def movie_list(request):
     # Create a query set: select some/all objects available in our database for a model
-    # movies = Movie.objects.all()
     movies = Movie.objects.all() # return a querySet
     movies_list = list(movies.values()) # .values convert the model instances into dictionaries, when is used as iterable
     
@@ -17,3 +16,14 @@ def movie_list(request):
     # These dictionaries has model field name as key and  model field value as a value, so we can use it as a response with JsonResponse
     return JsonResponse(data)
     
+    
+def movie_details(request, pk):
+    movie = Movie.objects.get(pk=pk) # Recall that these objects/instances will return its name property as states the str method in the model
+    
+    data = {
+        'name': movie.name,
+        'description': movie.description,
+        'active': movie.active
+    }
+    
+    return JsonResponse(data)
